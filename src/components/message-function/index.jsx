2d74b5bcd229b143c034993e2efcf9020d671a88
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-04-28 22:56:36 +0700

    add message list

--- src/components/message-function/index.jsx
+++ src/components/message-function/index.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import './style.sass';
 
-export const MessageFunction = ({text, setText}) => {
-	const [textValue, setTextValue] = useState(text);
+export const MessageFunction = ({messageList, setMessageList}) => {
+	const [textValue, setTextValue] = useState('');
+	const [author, setAuthor] = useState('');
 
-	return <div className="app__message message-function">
-		<input className="app__input message-function__input" type="text" value={textValue} onChange={e => setTextValue(e.target.value)}/>
-		<button className="message-function__button" type="button" onClick={() => setText(textValue)}>Отправить</button>
-	</div>;
+	return <form className="app__message message-function" action='' onSubmit={e => e.preventDefault()}>
+		<input placeholder="Автор" className="app__input message-function__input" type="text" value={author} onChange={e => setAuthor(e.target.value)}/>
+		<input placeholder="Сообщение" className="app__input message-function__input" type="text" value={textValue} onChange={e => setTextValue(e.target.value)}/>
+		<button className="message-function__button" type="button" onClick={() => {
+			setMessageList([{text: textValue, author: author}, ...messageList]);
+			setTextValue('');
+			setAuthor('');
+		}}>Отправить</button>
+	</form>;
 };
--- src/components/show-message-function/index.jsx
+++ src/components/show-message-function/index.jsx
@@ -1,6 +1,8 @@
-export const ShowMessageFunction = ({text}) => {
-	return <div>
-		<h3>Отправленное сообщение:</h3>
-		<div>{text}</div>
-	</div>;
+export const ShowMessageFunction = ({messageList}) => {
+	return <ul className='app__message-list'>{messageList.map((elem, index) => (
+		//TODO Индекс массива в качестве ключа - это не очень хорошая практика. Это временно, пока у сообщений нет ID 
+		<li key={index}>
+			<b>{elem.author}: </b><span>{elem.text}</span>
+		</li>
+	))}</ul>;
 };
