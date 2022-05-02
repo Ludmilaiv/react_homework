35553909e1c3875a1f446b3f624b7c05f2ca4e06
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-05-02 22:19:31 +0700

    add jest config, add button test

--- src/components/message-form-function/components/button/index.tsx
+++ src/components/message-form-function/components/button/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { IButtonProps } from './interface';
 
 export const Button: FC<IButtonProps> = ({className, content, type, disabled, onClick}) => {
-  return <button className={className} disabled={disabled} onClick={onClick} type={type}>
+  return <button style={{borderRadius: '3'}} className={className} disabled={disabled} onClick={onClick} type={type}>
     {content}
   </button>;
 };
