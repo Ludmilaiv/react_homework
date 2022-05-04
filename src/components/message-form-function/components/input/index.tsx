4353fb2f028ba1523a0dd6585210f47e29721bd9
Людмила <56251614+Ludmilaiv@users.noreply.github.com>
2022-05-04 16:19:32 +0700

    fix focus

--- src/components/message-form-function/components/input/index.tsx
+++ src/components/message-form-function/components/input/index.tsx
@@ -3,5 +3,5 @@ import { IInputProps } from './interface';
 import TextFieldUI from '@mui/material/TextField';
 
 export const Input: FC<IInputProps> = ({autofocus, placeholder, value, onChange}) => {
-  return <TextFieldUI inputRef={input => autofocus && input && input.focus()} autoFocus={autofocus} id='outlined-basic' onChange={onChange} value={value} label={placeholder} variant='outlined' />;
+  return <TextFieldUI autoFocus={autofocus} id='outlined-basic' onChange={onChange} value={value} label={placeholder} variant='outlined' />;
 };
